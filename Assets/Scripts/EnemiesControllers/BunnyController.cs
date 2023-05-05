6c534d31121611b445f588ab6dab51b8f682b217
OGabrielfo <ogabrielfo.silva@gmail.com>
2023-05-05 20:15:04 -0300

    Inimigos Adicionados
    Foram adicionados dois inimigos
    O Coelho que pula quando o player se aproxima
    A pedra que diminui quando atingida, mas ainda é necessário finalizar sua implantação. Falta corrigir a colisão dos limites de patrulha e alterar o tamanho do colisor de acordo com o tamanho da atual da pedra.
    
    Recomendada implantação do sistema de colisão e danos com o player para evitar retrabalho futuro em todos os inimigos.

---
+++ Assets/Scripts/EnemiesControllers/BunnyController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyController : MonoBehaviour
+{
+    public float life;
+    public float jumpForce;
+    public float distanciaMaximaDoPlayer;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float groundCheckRadius;
+
+    private Rigidbody2D _rb;
+    private Animator _anim;
+    private GameObject _player;
+    private int _jumpCount = 0;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //_distanciaAtualDoPlayer = Vector2.Distance(_player.transform.position, transform.position);
+        if(_player.transform.position.x >= _rb.position.x - distanciaMaximaDoPlayer && _player.transform.position.x <= _rb.position.x + distanciaMaximaDoPlayer && IsGrounded())
+        {
+            Jump();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        _anim.SetFloat("VerticalVelocity", _rb.velocity.y);
+        _anim.SetBool("Grounded", IsGrounded());
+    }
+
+    void Jump()
+    {
+        _jumpCount++;
+        _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        _anim.SetTrigger("Jump");
+    }
+
+    bool IsGrounded()
+    {
+        
+        if (Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
